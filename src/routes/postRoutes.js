9e1757e875fb5e2bf5f5f4ae608b721d19ce91c6
Mariana Islas <marianaislasm2004@gmail.com>
2025-11-11 15:58:36 -0700

    Feat: fixed git

---
+++ src/routes/postRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {
+    createPost,
+    getAllPosts,
+    deletePost
+    
+} from '../controllers/postController.js'
+
+const router = express.Router();
+
+router.post('/', createPost);
+router.get('/', getAllPosts);
+router.delete('/:id', deletePost);
+
+export default router;
