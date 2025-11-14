7d598d51ae46b9f82ca9df369f6c6a2a78b8aabd
Mariana Islas <marianaislasm2004@gmail.com>
2025-11-14 16:46:34 -0700

    Feat: fixed db

--- src/db.js
+++ src/db.js
@@ -2,28 +2,32 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = (text, params) => {
-    return pool.query(text, params);
+  return pool.query(text, params);
 };
 
-const initializeDatabase = async() => {
-    try{
-        await query(`
-        CREATE TABLE IF NOT EXISTS posts (
-            id SERIAL PRIMARY KEY,
-            content TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        `);
-        console.log('Database tables created or already exist.')
-    } catch(error) {
-        console.error('Error initializing the database', error);
-    }
-}
+const initializeDatabase = async () => {
+  try {
+    await query(`
+      CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+    console.log("Database tables created or already exist.");
+  } catch (error) {
+    console.error("Error initializing the database", error);
+  }
+};
 
 initializeDatabase();
 
